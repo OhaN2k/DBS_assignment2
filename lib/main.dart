6c6358b8e62f7f278d4d3b77715aa458e44484b6
OhaN2k <66206021+OhaN2k@users.noreply.github.com>
2020-12-07 19:57:11 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,94 @@
 import 'package:flutter/material.dart';
-import 'Manager View/view.dart';
-//import 'package:bloc/bloc.dart';
-//import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+import 'operationPage.dart';
 
-/*
-This is the root of our app
-Routing is used for easy testing and app screen access
-Theme is also defined here
- */
-
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'DB Project',
       theme: ThemeData(
-          fontFamily: 'QuickSand',
-          buttonTheme: ButtonThemeData(
-            minWidth: 0,
-            height: 0,
-            materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-          )),
-      routes: {
-        '/login': (context) => ManagerView(),
-      },
-      initialRoute: '/login',
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'ASSIGNMENT 2'),
     );
   }
 }
 
-class Home extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  int value = 7;
+  TextEditingController userNameTextEditingController = TextEditingController();
+  TextEditingController passwordTextEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fabric Database", style: TextStyle(fontSize: 30.0,),),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            TextFormField(
+              style: TextStyle(fontSize: 18.0, color: Colors.black),
+              controller: userNameTextEditingController,
+              decoration: InputDecoration(
+                  hintText: "Username",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                  focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.red)),
+                  filled: true,
+                  suffixIcon: IconButton(icon: Icon(Icons.clear, color: Colors.red), onPressed: ()=>print("Se xoa het chu di"),)
+              ),
+            ),
+            SizedBox(height: 5,),
+            TextFormField(
+              style: TextStyle(fontSize: 18.0, color: Colors.black),
+              controller: passwordTextEditingController,
+              decoration: InputDecoration(
+                  hintText: "Password",
+                  hintStyle: TextStyle(color: Colors.grey),
+                  enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+                  focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.red)),
+                  filled: true,
+                  suffixIcon: IconButton(icon: Icon(Icons.clear, color: Colors.red), onPressed: ()=>print("Se xoa het chu di"),)
+              ),
+            ),
+            FlatButton.icon(
+                onPressed: () {
+                  if (value > 0) {
+                    Navigator.push(context,MaterialPageRoute(builder: (context) => OperationPage()),
+                    );
+                  }
+                  else {
+                        print(value);
+                        print("Sai vai ca lon");
+                      }
+                  } ,
+                icon: Icon(Icons.home), label: Text("Dang nhap"))
+          ],
+        ),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
   }
 }
+
